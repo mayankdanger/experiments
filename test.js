1d2493e24609b1091fbdd524bb95596c4344864b
mayankdanger <mayankdanger@gmail.com>
2016-01-27 20:37:28 +0530

    converted to json Obj

--- test.js
+++ test.js
@@ -1,7 +1,17 @@
 str='<html clr ="sad sds" p="ada afs fafs" gg="da-da">';
 var attrRegex=/[\w]+\s*\=\s*"[^"]+"/g;
-
+jsonObj = "{";
+//console.log(jsonObj);
 var pairs=str.match(attrRegex);
-console.log(str.replace(/([\w]+)\s*\=\s*("[^"]+")/g,'"$1":$2,'));
+for(var i in pairs){
+	pairs[i] = pairs[i].replace(/([\w]+)\s*\=\s*("[^"]+")/g,'"$1":$2');
+	if(i!=0)
+		jsonObj += ",";
+	jsonObj += pairs[i];
+}
+jsonObj += "}";
+var jsonObj = JSON.parse(jsonObj);
+console.log(jsonObj);
+
 
 
